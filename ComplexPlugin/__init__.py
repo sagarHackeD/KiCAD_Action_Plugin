c7e654e3497a7901e68a2a5e4e6dd6c2e91cd4b3
sagarHackeD <sagarnaik430@googlemail.com>
2024-02-03 20:23:59 +0530

    added boiler code

--- ComplexPlugin/__init__.py
+++ ComplexPlugin/__init__.py
@@ -1,1 +1,4 @@
 # This file is executed when the package is imported (on PCB editor startup)
+
+from .complex_plugin_action import ComplexPluginAction # Note the relative import!
+ComplexPluginAction().register() # Instantiate and register to PCB editor
--- ComplexPlugin/complex_plugin_action.py
+++ ComplexPlugin/complex_plugin_action.py
@@ -1,1 +1,16 @@
 # The ActionPlugin derived class lives here
+
+import pcbnew
+import os
+
+class ComplexPluginAction(pcbnew.ActionPlugin):
+    def defaults(self):
+        self.name = "A complex action plugin"
+        self.category = "A descriptive category name"
+        self.description = "A description of the plugin"
+        self.show_toolbar_button = True # Optional, defaults to False
+        self.icon_file_name = os.path.join(os.path.dirname(__file__), 'icon.png') # Optional
+
+    def Run(self):
+        # The entry function of the plugin that is executed on user action
+        print("Hello World")
